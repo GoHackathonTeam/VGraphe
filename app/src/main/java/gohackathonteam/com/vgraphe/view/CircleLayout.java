7f01421c3bc8f69b6ab91a8a5d4762a9a81ba54b
RomaSV <sve4nikov.roma@yandex.ru>
2018-04-25 21:40:33 +0300

    add friends activity

---
+++ app/src/main/java/gohackathonteam/com/vgraphe/view/CircleLayout.java
@@ -0,0 +1,17 @@
+package gohackathonteam.com.vgraphe.view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+
+public class CircleLayout extends RelativeLayout {
+
+    public CircleLayout(Context context){
+        super(context);
+    }
+
+    public void setCircle(int circle) {
+
+    }
+}
---
+++ app/src/main/java/gohackathonteam/com/vgraphe/view/FriendsFragment.java
@@ -0,0 +1,84 @@
+package gohackathonteam.com.vgraphe.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import gohackathonteam.com.vgraphe.R;
+import gohackathonteam.com.vgraphe.model.SocialGraph;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FriendsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FriendsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendsFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    private SocialGraph graph;
+    private Activity activity;
+    private ImageView personPhotoView;
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static FriendsFragment newInstance() {
+        FriendsFragment fragment = new FriendsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        activity = getActivity();
+
+        personPhotoView = activity.findViewById(R.id.personPhoto);
+        Bitmap img = graph.getCurrentPerson().getPhoto();
+        personPhotoView.setImageDrawable(new BitmapDrawable(getResources(), img));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
